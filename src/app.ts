eb8beec5987d4564f74f5bc5f051f55fe04b2370
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-23 19:33:43 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { ROUTER } from './routes';
+import cors from 'cors';
 
 export default function createApp() {
     const app = express();
 
     app.use(express.json());
     app.use("/api", ROUTER);
+
+    app.use(cors());
 
     return app;
 }
